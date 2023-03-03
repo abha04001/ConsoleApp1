49c8038cf607f1240b3f3127e14e13ec3c3884bb
Elev <Elev@TEB125103>
2023-03-03 14:41:46 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace upggift_6._2
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int strostaTalet = Storsta(5, 9);
+            Console.WriteLine("det är största talet är " + strostaTalet);
+
+        }
+        static int Storsta(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+                return tal1;
+
+
+            else
+            {
+                return tal2;
+            }
+        
+        }
+     
+
+            
+    }
+}
